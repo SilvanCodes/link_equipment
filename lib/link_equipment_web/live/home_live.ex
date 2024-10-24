96580b10ba9ed773eaaa68e7cb929f1619892c2f
Silvan Buedenbender <silvancodes@gmail.com>
2024-10-24 12:55:49 +0000

    Implement basic functionality

--- lib/link_equipment_web/live/home_live.ex
+++ lib/link_equipment_web/live/home_live.ex
@@ -2,18 +2,83 @@ defmodule LinkEquipmentWeb.HomeLive do
   @moduledoc false
   use LinkEquipmentWeb, :live_view
 
+  import Util.Phoenix
+  import Util.Result
+
+  def mount(_params, _session, socket) do
+    socket
+    |> assign(form: to_form(%{}), results: nil)
+    |> ok()
+  end
+
+  def handle_params(%{"url_input" => url_input} = _params, _uri, socket) do
+    socket =
+      with {:ok, uri} <- URI.new(url_input),
+           {:ok, uri} <- validate_as_remote_uri(uri) do
+        socket = assign(socket, :form, to_form(%{"url_input" => url_input}))
+
+        if socket.assigns.live_action == :scan do
+          assign_async(socket, :results, fn -> scan_url(uri) end)
+        else
+          socket
+        end
+      else
+        {:error, error} ->
+          assign(socket, :form, to_form(%{"url_input" => url_input}, errors: [url_input: {error, []}]))
+      end
+
+    noreply(socket)
+  end
+
+  def handle_params(_params, _uri, socket) do
+    noreply(socket)
+  end
+
+  def handle_event("validate", params, socket) do
+    socket
+    |> push_patch(to: ~p"/?#{Map.take(params, ["url_input"])}", replace: true)
+    |> noreply()
+  end
+
+  def handle_event("scan", params, socket) do
+    socket
+    |> push_patch(to: ~p"/scan?#{params}")
+    |> noreply()
+  end
+
   def render(assigns) do
     ~H"""
-    <ul>
-      <li>1</li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-      <li>5</li>
-      <li>6</li>
-      <li>7</li>
-      <li>8</li>
-    </ul>
+    <.stack>
+      <.form for={@form} phx-change="validate" phx-submit="scan">
+        <.cluster>
+          <.input type="text" field={@form[:url_input]} label="URL" />
+          <.button>Scan</.button>
+        </.cluster>
+      </.form>
+      <div :if={@results && @results.loading}>Scanning...</div>
+      <.stack :if={results = @results && @results.ok? && @results.result}>
+        <p>Last Results</p>
+        <.stack>
+          <li :for={result <- results}>
+            <.link href={~p"/scan?#{%{"url_input" => URI.to_string(result.url)}}"}>
+              <%= result.url %>
+            </.link>
+          </li>
+        </.stack>
+      </.stack>
+    </.stack>
     """
   end
+
+  defp scan_url(uri) do
+    with {:ok, results} <- LinkEquipment.Lychee.collect_links(URI.to_string(uri)) do
+      {:ok, %{results: results}}
+    end
+  end
+
+  defp validate_as_remote_uri(%URI{scheme: nil}), do: {:error, :scheme_missing}
+  defp validate_as_remote_uri(%URI{scheme: ""}), do: {:error, :scheme_missing}
+  defp validate_as_remote_uri(%URI{host: nil}), do: {:error, :host_missing}
+  defp validate_as_remote_uri(%URI{host: ""}), do: {:error, :host_missing}
+  defp validate_as_remote_uri(%URI{} = uri), do: {:ok, uri}
 end
--- lib/link_equipment_web/router.ex
+++ lib/link_equipment_web/router.ex
@@ -18,6 +18,8 @@ defmodule LinkEquipmentWeb.Router do
     pipe_through :browser
 
     live "/", HomeLive, :default
+
+    live "/scan", HomeLive, :scan
   end
 
   # Other scopes may use custom stacks.
