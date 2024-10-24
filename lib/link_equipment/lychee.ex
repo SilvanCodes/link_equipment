d381a8fd0e149ee54c3e900642c26b581d19a133
Silvan Buedenbender <silvancodes@gmail.com>
2024-10-24 08:54:05 +0000

    Spec function

--- lib/link_equipment/lychee.ex
+++ lib/link_equipment/lychee.ex
@@ -5,5 +5,6 @@ defmodule LinkEquipment.Lychee do
     crate: :linkequipment_lychee
 
   # When your NIF is loaded, it will override this function.
+  @spec collect_links(String.t()) :: list(LinkEquipment.Link.t())
   def collect_links(_url), do: :erlang.nif_error(:nif_not_loaded)
 end
