90ec2539e0ff8e584a18caac939b2d4dadd7439c
Silvan Buedenbender <silvancodes@gmail.com>
2024-10-25 11:59:57 +0000

    Fix typespec

--- lib/link_equipment/lychee.ex
+++ lib/link_equipment/lychee.ex
@@ -4,7 +4,9 @@ defmodule LinkEquipment.Lychee do
     otp_app: :link_equipment,
     crate: :linkequipment_lychee
 
+  alias Util.Result
+
   # When your NIF is loaded, it will override this function.
-  @spec collect_links(String.t()) :: list(LinkEquipment.Link.t())
+  @spec collect_links(String.t()) :: Result.t(list(LinkEquipment.Link.t()))
   def collect_links(_url), do: :erlang.nif_error(:nif_not_loaded)
 end
